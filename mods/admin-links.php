345f481c830dab54b8153f90e476d10f54c361bf
Zdenek Travnicek <v154c1@gmail.com>
2024-04-03 14:33:26 +0200

    Add entries page to admin-links

--- mods/admin-links.php
+++ mods/admin-links.php
@@ -1,3 +1,4 @@
+<a href="mods/entries.php">Entries</a><br>
 <a href="mods/evaluation.php">EVALUATION</a><br>
 <a href="mods/page.php">RESULTS</a><br>
 <a href="mods/codes.php">QR codes for entries</a>
