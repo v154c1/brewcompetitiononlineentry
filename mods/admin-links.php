ed136a9bbcc5eac7eb43abe47484dc132604ceb7
Zdenek Travnicek <v154c1@gmail.com>
2024-05-07 22:08:52 +0200

    Add status page

--- mods/admin-links.php
+++ mods/admin-links.php
@@ -1,4 +1,5 @@
 <a href="mods/entries.php">Entries</a><br>
 <a href="mods/evaluation.php">EVALUATION</a><br>
 <a href="mods/page.php">RESULTS</a><br>
+<a href="mods/status.php">Status</a><br>
 <a href="mods/codes.php">QR codes for entries</a>
