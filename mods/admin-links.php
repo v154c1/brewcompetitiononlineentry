2fe662ba3210095ca99032eede10884d4f3c9053
Zdenek Travnicek <v154c1@gmail.com>
2026-05-09 02:26:32 +0200

    refactor: simplify admin links by consolidating into a single Cech admin entry

--- mods/admin-links.php
+++ mods/admin-links.php
@@ -1,6 +1,1 @@
-<a href="mods/entries.php">Entries</a><br>
-<a href="mods/evaluation.php">EVALUATION</a><br>
-<a href="mods/page.php">RESULTS</a><br>
-<a href="mods/status.php">Status</a><br>
-<a href="mods/codes.php">QR codes for entries</a><br>
-<a href="mods/table_notes.php">Table notes</a>
+<h1><a href="mods/cech_admin.php">Cech admin</a></h1>
